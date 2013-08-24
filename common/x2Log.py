35ccce2fb1e28be3443e4658f3a13a147ffb2121
dinimicky <dinimicky@gmail.com>
2013-08-25 07:25:59 +0800

    add log function to record the X2 message received by x2Server

--- common/x2Log.py
+++ common/x2Log.py
@@ -4,5 +4,25 @@ Created on 2013-8-24
 @author: Brilliant
 '''
 class log(object):
-    pass
-    
+    def __init__(self, logHandler):
+        self.logHandler = logHandler
+        self.Counter = 1
+        
+    def msg(self, content=""):
+        head = "=="*10+"START"+"=="*10
+        tail = "=="*10+"=END="+"=="*10
+        self.logHandler.write("\n%d:%s\n" % (self.Counter, head))
+        for line in content.split('\n'):
+            if line != "":
+                self.logHandler.write("%d:%s\n" % (self.Counter, line))
+        self.logHandler.write("%d:%s\n" % (self.Counter, tail))
+        self.Counter += 1
+        
+        
+
+
+fd = open("test.log", "w")
+test = log(fd)
+test.msg("test1\ntest2\n")
+test.msg("test3\ntest4\n")  
+fd.close()  
