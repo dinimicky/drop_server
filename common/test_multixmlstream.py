2d5434a9c4112906d8c7237e54e80d5b0e7467f9
ezonghu <zongjie.hu@ericsson.com>
2014-06-25 16:45:56 +0200

    add '-' in test_multixmlstream to check if the xpath can process it
    change the x2Server structure
    
    but maybe we need to change it again.

--- common/test_multixmlstream.py
+++ common/test_multixmlstream.py
@@ -26,12 +26,12 @@ class MultiXmlStreamTest(unittest.TestCase):
         def storeElement(xmlInstance):
             xmlString.append(xmlInstance)
         
-        self.multixmlstream.addObserver(XPathQuery("/body/tl"), storeElement)
+        self.multixmlstream.addObserver(XPathQuery("/body/tl-1"), storeElement)
         self.multixmlstream.connectionMade()
-        self.multixmlstream.dataReceived("<root><body><hd/></body><body><tl><bb1/></tl></body>")
-        self.multixmlstream.dataReceived('<body><tl><bb2/>Hi</tl></body></root>')
+        self.multixmlstream.dataReceived("<root><body><hd/></body><body><tl-1><bb1/></tl-1></body>")
+        self.multixmlstream.dataReceived('<body><tl-1><bb2/>Hi</tl-1></body></root>')
         self.assertEqual(2, len(xmlString))
-        self.assertEqual('tl', xmlString[0].children[0].name)
+        self.assertEqual('tl-1', xmlString[0].children[0].name)
         self.assertEqual('bb1', xmlString[0].children[0].children[0].name)
         self.assertEqual('bb2', xmlString[1].children[0].children[0].name)
 
--- li/x2Server.py
+++ li/x2Server.py
@@ -3,50 +3,74 @@ Created on 2013-2-25
 
 @author: ezonghu
 '''
+
+'''
+X2Server main function:
+1. auto reply the Ping request
+2. record the Notification from MGC into some log files, there is a flag to control whether to record or not.
+'''
 from twisted.internet.protocol import ServerFactory
 from twisted.python import log
+from twisted.words.xish import xmlstream   
 from common.multixmlstream import MultiXmlStream
-from common.multixmlstream import generateXmlFromList as toXML
+from twisted.words.xish.xpath import XPathQuery
 from common import config
 from common.state import Ping
 from common.log import X2Log
 from lixml import li_xml_temp
 class X2ServerProtocol(MultiXmlStream):
+    X2PingReqPath = XPathQuery('/payload/ping/pingType/pingRequest')
+    getX2PingReqSeqNbr = XPathQuery('/payload/ping/seqNbr').queryForString
+    X2IRIEvent = XPathQuery('/payload/extPDU/IRI-Event')
+    def _genPingResp(self, element):
+        SeqNbr = int(X2ServerProtocol.getX2PingReqSeqNbr(element))
+        self.pingResp = li_xml_temp.pingX2Resp(SeqNbr)
+        self.addOnetimeObserver(xmlstream.STREAM_END_EVENT, self._sendPingResp)
+        
+    def _getIRIEvent(self, element):
+        if self.factory.x2LogHandler is not None:
+            self.addOnetimeObserver(xmlstream.STREAM_END_EVENT, self._recordIRIEvent)
+        
+    def _recordIRIEvent(self):
+        self.factory.x2LogHandler.msg(self.reqRootElement.toXml())
+        self.reqRootElement = None
+        
+    def _sendPingResp(self, obj):
+        print obj,self
+        if self.pingResp is not None:
+            self.send(self.pingResp)
+            self.pingResp = None
+        self.reqRootElement = None
     def __init__(self):
-        self.reqMsg = None
+        self.reqRootElement = None
+        self.pingResp = None
         MultiXmlStream.__init__(self)
-
+    
     def connectionMade(self):
         log.msg('x2 connection is established')
+        self.addObserver(X2ServerProtocol.X2PingReqPath, self._genPingResp)
+        self.addObserver(X2ServerProtocol.X2IRIEvent, self._getIRIEvent)    
         MultiXmlStream.connectionMade(self)
         
-    def onDocumentEnd(self):
-        Elements = self.Elements
-        if 'pingRequest' in str(Elements):
-            log.msg('recv pingRequest')
-            (_, Payloads) = self.check_tuple('payload', Elements)
-            Payload = Payloads[0]
-            log.msg('get the payload:', Payload)
-            (_, Ping) = Payload[0]
-            log.msg('get Ping', Ping)
-            (_Result, Seq) = self.check_tuple('seqNbr', Ping)
-            log.msg('get Seq:', Seq)
-            Seq = int(Seq[0][0])                         
-
-            Resp = li_xml_temp.pingX2Resp(Seq)     
-            self.send(Resp)
-            log.msg('send out ping resp:', Resp)
-        else:#SipMessage
-            log.msg('recv X2 Notify Message:', toXML(Elements))
-            self.factory.x2LogHandler.msg(toXML([('LIC-Msg', Elements)]))
-        MultiXmlStream.onDocumentEnd(self)
+    def onDocumentStart(self, rootElement):
+        self.reqRootElement = rootElement
+        MultiXmlStream.onDocumentStart(self, rootElement)
+    
+    def onElement(self, element):
+        self.reqRootElement.addElement(element)
+        MultiXmlStream.onElement(self, element)
+        
+        
             
 class X2ServerFactory(ServerFactory):
     protocol = X2ServerProtocol
     def __init__(self):
         import os.path
-        Dir, Fn = os.path.split(config.x2InterfaceLog)
-        self.x2LogHandler = X2Log(Fn, Dir)
+        if config.x2InterfaceLog is not None:
+            Dir, Fn = os.path.split(config.x2InterfaceLog)
+            self.x2LogHandler = X2Log(Fn, Dir)
+        else:
+            self.x2LogHandler = None
         
     
         
